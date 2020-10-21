4ed13e1f6a607625b58b9def69bb85b37158ac73
James Mernin <jamespmernin@gmail.com>
2020-10-21 12:16:42 -0400

    Solved algorithm

--- sharingElements.js
+++ sharingElements.js
@@ -1,7 +1,12 @@
 //SHARING ELEMENTS
+// Solution adapted from here: https://stackoverflow.com/questions/8495687/split-array-into-chunks
 
 const sharingElements = (arr, size) => {
-
+  return [].concat.apply([],
+    arr.map(function (elem, i) {
+      return i % size ? [] : [arr.slice(i, i + size)];
+    })
+  );
 }
 
 console.log(sharingElements(["a", "b", "c", "d"], 3))
